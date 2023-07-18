488a354ea1d9d62bdc52319a531ee84f56f3b491
Afifah <111559790+mbkfah@users.noreply.github.com>
2023-07-18 18:51:56 +0700

    Update Page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
 import { Editor } from "./components/editor";
 
 async function getAllNotes() {
-  const res = await fetch("http://localhost:3001/api/v1/notes", {
-    cache: "no-cache",
+  const res = await fetch("http://localhost:3000/api/v1/notes", {
+    cache: "no-store",
   });
   const data = await res.json();
   return data;
